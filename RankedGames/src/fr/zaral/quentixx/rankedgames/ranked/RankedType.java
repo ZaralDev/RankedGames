1b5d706117f98ec1523cf147c2bf3830975ee66d
Quentin <nelsonsims@hotmail.fr>
2016-01-17 16:47:18 +0100

    Clean code
    Clean code

--- RankedGames/src/fr/zaral/quentixx/rankedgames/ranked/RankedType.java
+++ RankedGames/src/fr/zaral/quentixx/rankedgames/ranked/RankedType.java
@@ -2,36 +2,30 @@ package fr.zaral.quentixx.rankedgames.ranked;
 
 import fr.zaral.quentixx.rankedgames.Main;
 
-public class RankedType
-{
-  private int type;
-  private RankedQueue queue;
+public class RankedType {
+	
+	public static RankedType get(int type) {
+		for (RankedType t : Main.types)
+			if (t.getType() == type)
+				return t;
+		return null;
+	}
+	
+	private int type;
+	private RankedQueue queue;
   
-  public RankedType(int type)
-  {
-    this.type = type;
-    this.queue = new RankedQueue();
-    this.queue.setType(this);
-    this.queue.setSlots(type * 2);
-  }
+	public RankedType(int type) {
+		this.type = type;
+		queue = new RankedQueue();
+		queue.setType(this);
+		queue.setSlots(type * 2);
+	}
   
-  public RankedQueue getQueue()
-  {
-    return this.queue;
-  }
+	public RankedQueue getQueue() {
+		return this.queue;
+	}
   
-  public static RankedType get(int type)
-  {
-    for (RankedType t : Main.types) {
-      if (t.getType() == type) {
-        return t;
-      }
-    }
-    return null;
-  }
-  
-  public int getType()
-  {
-    return this.type;
-  }
+	public int getType() {
+		return this.type;
+	}
 }
