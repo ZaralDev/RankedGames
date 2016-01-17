8b888e216c489dbba5f683aadc87f05437afbded
Quentin <nelsonsims@hotmail.fr>
2016-01-17 16:43:42 +0100

    Clean code
    Clean code

--- RankedGames/src/fr/zaral/quentixx/rankedgames/utils/ConfigAccessor.java
+++ RankedGames/src/fr/zaral/quentixx/rankedgames/utils/ConfigAccessor.java
@@ -9,69 +9,55 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class ConfigAccessor
-{
-  private final String fileName;
-  private final JavaPlugin plugin;
-  private File configFile;
-  private FileConfiguration fileConfiguration;
+public class ConfigAccessor {
+	
+	private final String fileName;
+	private final JavaPlugin plugin;
+	private File configFile;
+	private FileConfiguration fileConfiguration;
   
-  public ConfigAccessor(JavaPlugin plugin, String fileName)
-  {
-    if (plugin == null) {
-      throw new IllegalArgumentException("plugin cannot be null");
-    }
-    if (!plugin.isInitialized()) {
-      throw new IllegalArgumentException("plugin must be initialized");
-    }
-    this.plugin = plugin;
-    this.fileName = fileName;
-    File dataFolder = plugin.getDataFolder();
-    if (dataFolder == null) {
-      throw new IllegalStateException();
-    }
-    this.configFile = new File(plugin.getDataFolder(), fileName);
-  }
+	@SuppressWarnings("deprecation")
+	public ConfigAccessor(JavaPlugin plugin, String fileName) {
+		if (plugin == null)
+	      throw new IllegalArgumentException("plugin cannot be null");
+		if (!plugin.isInitialized())
+			throw new IllegalArgumentException("plugin must be initialized");
+		this.plugin = plugin;
+		this.fileName = fileName;
+		File dataFolder = plugin.getDataFolder();
+		if (dataFolder == null)
+			throw new IllegalStateException();
+		configFile = new File(plugin.getDataFolder(), fileName);
+	}
   
-  public void reloadConfig()
-  {
-    this.fileConfiguration = YamlConfiguration.loadConfiguration(this.configFile);
-    
-    InputStream defConfigStream = this.plugin.getResource(this.fileName);
-    if (defConfigStream != null)
-    {
-      YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(defConfigStream);
-      this.fileConfiguration.setDefaults(defConfig);
-    }
-  }
+	public void reloadConfig() {
+		fileConfiguration = YamlConfiguration.loadConfiguration(this.configFile);
+		InputStream defConfigStream = this.plugin.getResource(this.fileName);
+		if (defConfigStream != null) {
+			@SuppressWarnings("deprecation")
+			YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(defConfigStream);
+			fileConfiguration.setDefaults(defConfig);
+		}
+	}
   
-  public FileConfiguration getConfig()
-  {
-    if (this.fileConfiguration == null) {
-      reloadConfig();
-    }
-    return this.fileConfiguration;
-  }
+	public FileConfiguration getConfig() {
+		if (fileConfiguration == null)
+			reloadConfig();
+		return this.fileConfiguration;
+	}
   
-  public void saveConfig()
-  {
-    if ((this.fileConfiguration == null) || (this.configFile == null)) {
+	public void saveConfig() {
+		if ((fileConfiguration == null) || (configFile == null))
       return;
-    }
-    try
-    {
-      getConfig().save(this.configFile);
-    }
-    catch (IOException ex)
-    {
-      this.plugin.getLogger().log(Level.SEVERE, "Could not save config to " + this.configFile, ex);
-    }
-  }
+		try {
+			getConfig().save(this.configFile);
+		} catch (IOException ex) {
+			plugin.getLogger().log(Level.SEVERE, "Could not save config to " + this.configFile, ex);
+		}
+	}
   
-  public void saveDefaultConfig()
-  {
-    if (!this.configFile.exists()) {
-      this.plugin.saveResource(this.fileName, false);
-    }
-  }
+	public void saveDefaultConfig() {
+		if (!configFile.exists())
+			plugin.saveResource(fileName, false);
+	}
 }
