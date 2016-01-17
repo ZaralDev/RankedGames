c7cae19fcdef0e057c4d1cd1626cca55e5909054
Quentin <nelsonsims@hotmail.fr>
2016-01-17 16:42:34 +0100

    Clean code
    Clean code

--- RankedGames/src/fr/zaral/quentixx/rankedgames/SpamLevel.java
+++ RankedGames/src/fr/zaral/quentixx/rankedgames/SpamLevel.java
@@ -7,48 +7,37 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitTask;
 
-public class SpamLevel
-  implements Runnable
-{
-  BukkitTask task;
-  private static HashMap<String, Integer> level;
+public class SpamLevel implements Runnable {
+	
+	public BukkitTask task;
+	private static HashMap<String, Integer> level;
   
-  public SpamLevel()
-  {
-    this.task = Bukkit.getServer().getScheduler().runTaskTimer(Main.plugin, this, 0L, 40L);
-    level = new HashMap();
-  }
+	public SpamLevel() {
+		this.task = Bukkit.getServer().getScheduler().runTaskTimer(Main.plugin, this, 0L, 40L);
+		level = new HashMap<>();
+	}
   
-  public void run()
-  {
-    for (Player player : Bukkit.getServer().getOnlinePlayers()) {
-      if (level.containsKey(player.getName())) {
-        level.remove(player.getName());
-      }
-    }
-  }
+	public void run() {
+		for (Player player : Bukkit.getServer().getOnlinePlayers())
+			if (level.containsKey(player.getName()))
+				level.remove(player.getName());
+	}
   
-  public static void put(Player player)
-  {
-    level.put(player.getName(), Integer.valueOf(level.containsKey(player.getName()) ? ((Integer)level.get(player.getName())).intValue() + 1 : 1));
-  }
+	public static void put(Player player) {
+		level.put(player.getName(), Integer.valueOf(level.containsKey(player.getName()) ? ((Integer)level.get(player.getName())).intValue() + 1 : 1));
+	}
   
-  public static boolean contains(Player player)
-  {
-    return level.containsKey(player.getName());
-  }
+	public static boolean contains(Player player) {
+		return level.containsKey(player.getName());
+	}
   
-  public static boolean isSpammed(Player player)
-  {
-    if ((contains(player)) && 
-      (((Integer)level.get(player.getName())).intValue() > 1)) {
-      return true;
-    }
-    return false;
-  }
+	public static boolean isSpam(Player player) {
+		if ((contains(player)) && (((Integer)level.get(player.getName())).intValue() > 1))
+			return true;
+		return false;
+	}
   
-  public static String getMessage()
-  {
-    return ChatColor.RED + "Merci de ne pas spam!";
-  }
+	public static String getMessage() {
+		return ChatColor.RED + "Merci de ne pas spam!";
+	}
 }
