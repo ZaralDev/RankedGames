4267816e6958529e895a05df6b7ea2ac52561790
Quentin <nelsonsims@hotmail.fr>
2016-01-17 16:49:59 +0100

    Clean code
    Clean code

--- RankedGames/src/fr/zaral/quentixx/rankedgames/event/PlayerLeaveRankedEvent.java
+++ RankedGames/src/fr/zaral/quentixx/rankedgames/event/PlayerLeaveRankedEvent.java
@@ -6,37 +6,31 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.player.PlayerEvent;
 
-public class PlayerLeaveRankedEvent
-  extends PlayerEvent
-{
-  private static final HandlerList handlers = new HandlerList();
-  private Ranked ranked;
-  private RankedPlayer rankedPlayer;
+public class PlayerLeaveRankedEvent extends PlayerEvent {
+	
+	private static final HandlerList handlers = new HandlerList();
+	private Ranked ranked;
+	private RankedPlayer rankedPlayer;
   
-  public PlayerLeaveRankedEvent(Player who, Ranked ranked)
-  {
-    super(who);
-    this.rankedPlayer = RankedPlayer.get(who.getName());
-    this.ranked = ranked;
-  }
+	public PlayerLeaveRankedEvent(Player who, Ranked ranked) {
+		super(who);
+		this.rankedPlayer = RankedPlayer.get(who.getName());
+		this.ranked = ranked;
+	}
   
-  public Ranked getRanked()
-  {
-    return this.ranked;
-  }
+	public Ranked getRanked() {
+		return this.ranked;
+	}
   
-  public RankedPlayer getRankedPlayer()
-  {
-    return this.rankedPlayer;
-  }
+	public RankedPlayer getRankedPlayer() {
+		return this.rankedPlayer;
+	}
+	
+	public HandlerList getHandlers() {
+		return handlers;
+	}
   
-  public HandlerList getHandlers()
-  {
-    return handlers;
-  }
-  
-  public static HandlerList getHandlerList()
-  {
-    return handlers;
-  }
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
 }
