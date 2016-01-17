1c65e4d67fbc28f926c68fb383778d11843f506e
Quentin <nelsonsims@hotmail.fr>
2016-01-17 16:43:07 +0100

    Clean code
    Clean code

--- RankedGames/src/fr/zaral/quentixx/rankedgames/utils/CodeUtils.java
+++ RankedGames/src/fr/zaral/quentixx/rankedgames/utils/CodeUtils.java
@@ -1,22 +1,30 @@
 package fr.zaral.quentixx.rankedgames.utils;
 
 import java.util.Random;
-import org.bukkit.Bukkit;
-import org.bukkit.Server;
 
-public class CodeUtils
-{
-  public static int randomInt(int min, int max)
-  {
-    Random rand = new Random();
-    
-    int randomNum = rand.nextInt(max - min + 1) + min;
-    
-    return randomNum;
-  }
+import org.bukkit.Bukkit;
+
+public class CodeUtils {
+	
+	public static int randomInt(int min, int max) {
+		Random rand = new Random();
+		int randomNum = rand.nextInt(max - min + 1) + min;
+		return randomNum;
+	}
   
-  public static void runConsoleCommand(String command)
-  {
-    Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), command);
-  }
+	public static void runConsoleCommand(String command) {
+		Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), command);
+	}
+	
+	public static int getIdealInvSize(int needed) {
+		int i = 9;
+		int count = 1;
+		while (i != 54) {
+			if (needed <= i)
+				return i;
+			count++;
+			i *= count;
+		}
+		return 9;
+	}
 }
