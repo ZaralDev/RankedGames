6481a28d07ae27f3012e74cbc412b9430d34ed1b
Quentin <nelsonsims@hotmail.fr>
2016-01-17 16:45:25 +0100

    Clean code
    Clean code

--- RankedGames/src/fr/zaral/quentixx/rankedgames/utils/PlayerUtils.java
+++ RankedGames/src/fr/zaral/quentixx/rankedgames/utils/PlayerUtils.java
@@ -5,22 +5,20 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.potion.PotionEffect;
 
-public class PlayerUtils
-{
-  public static void goNaked(Player player)
-  {
-    PlayerInventory inv = player.getInventory();
-    inv.setArmorContents(new ItemStack[4]);
-    inv.clear();
-    player.setFoodLevel(20);
-    player.setBedSpawnLocation(null);
-    player.setHealth(20.0D);
-    player.setFireTicks(0);
-    player.setExp(0.0F);
-    player.setLevel(0);
-    player.setFallDistance(0.0F);
-    for (PotionEffect effect : player.getActivePotionEffects()) {
-      player.removePotionEffect(effect.getType());
-    }
-  }
+public class PlayerUtils {
+	
+	public static void goNaked(Player player) {
+		PlayerInventory inv = player.getInventory();
+		inv.setArmorContents(new ItemStack[4]);
+		inv.clear();
+		player.setFoodLevel(20);
+		player.setBedSpawnLocation(null);
+		player.setHealth(20.0D);
+		player.setFireTicks(0);
+		player.setExp(0.0F);
+		player.setLevel(0);
+		player.setFallDistance(0.0F);
+		for (PotionEffect effect : player.getActivePotionEffects())
+			player.removePotionEffect(effect.getType());
+	}
 }
