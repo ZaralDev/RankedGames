325f1f2c56d1a2a5ba18637482835e2436ed83bc
Quentin <nelsonsims@hotmail.fr>
2016-01-17 19:04:05 +0100

    Database methods added

--- src/fr/zaral/quentixx/rankedgames/Database.java
+++ src/fr/zaral/quentixx/rankedgames/Database.java
@@ -2,6 +2,7 @@ package fr.zaral.quentixx.rankedgames;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.logging.Level;
 
@@ -66,5 +67,35 @@ public class Database {
 		} catch (SQLException ex) {
 			Bukkit.getServer().getLogger().log(Level.SEVERE, "Can not close database: " + ex.getMessage());
 		}
+	}
+	
+	protected static PreparedStatement createStatement(String query) {
+		if (!isConnected())
+			Database.connect();
+		try {
+			return Database.connection.prepareStatement(query);
+		} catch (SQLException ex) {
+			Bukkit.getServer().getLogger().warning("An SQL error occured: " + ex.getMessage());
+		}
+		return null;
+	}
+	
+	public static boolean isRegistredUser(String name) throws SQLException {
+		// TODO: A FAIRE
+		return false;
+	}
+	
+	public static void registerUser(String name) throws SQLException {
+		// TODO: A FAIRE
+	}
+	
+	public static int getRatio(String name) throws SQLException {
+		// TODO: A FAIRE
+		return 0;
+	}
+	
+	public static int updateRatio(String name, int ratio) throws SQLException {
+		// TODO: A FAIRE
+		return ratio;
 	}
 }
